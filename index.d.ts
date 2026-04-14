37df5b9ea5a4ea3780d79480c1782ac1480a0384
fbwoolf <6493321+fbwoolf@users.noreply.github.com>
2026-04-14 10:48:25 -0400

    fix: exported types

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export { ProofHandle, VerifyResult, deserialize_proof_bytes, init_defaults, init_with, main, verify_proof } from './pkg/airbender_wasm_stark_verifier.js';
+
+export function verify_stark(proof_bytes: Uint8Array, vk_bytes?: [Uint8Array, Uint8Array] | null): boolean;
+
+export default function init(): Promise<void>;
